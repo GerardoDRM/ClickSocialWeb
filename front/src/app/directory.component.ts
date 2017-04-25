a56a5ae31ed3eb4f41b720440c268e211a1cf2cf
GerardoDRM <gerardo.delarosama@udlap.mx>
2017-04-24 19:21:20 -0500

    Adding organizations consumption

--- front/src/app/directory.component.ts
+++ front/src/app/directory.component.ts
@@ -2,18 +2,42 @@ import { Component } from '@angular/core';
 import { ElementRef } from "@angular/core";
 import { ViewChild } from "@angular/core";
 import { AfterViewInit } from "@angular/core";
+import { organizationService } from './directory.service';
 
 declare var dialogPolyfill: any;
 
+export interface Organization {
+  _id:string,
+  model:string,
+  entity:string,
+  social_group:string,
+  federal_entity:string,
+  geo_issue:string,
+  web:string,
+  email:string,
+  type:string
+ }
 
 @Component({
   templateUrl: './directory.component.html',
-  styleUrls: ['./main.css']
+  styleUrls: ['./main.css'],
+  providers: [organizationService]
 })
 export class DirectoryComponent implements AfterViewInit {
-  constructor(private _elRef: ElementRef) {
+  organizations: Organization[];
+  mode = 'Observable';
+
+  constructor(private _elRef: ElementRef, private challengesService: organizationService) {
   }
   ngAfterViewInit() {
 
+  }
+  ngOnInit() {
+    this.getOrganizations();
+  }
+  getOrganizations() {
+    this.challengesService.getData().subscribe(
+      response => this.organizations = response
+    )
   }
 }
---
+++ front/src/app/directory.service.ts
@@ -0,0 +1,45 @@
+import { Injectable }              from '@angular/core';
+import { Http, Response }          from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+export interface Organization {
+  _id:string,
+  model:string,
+  entity:string,
+  social_group:string,
+  federal_entity:string,
+  geo_issue:string,
+  web:string,
+  email:string,
+  type:string
+ }
+
+@Injectable()
+export class organizationService {
+  private orgUrl = 'http://localhost:5000/api/v0/organizations';  // URL to web API
+  constructor(private http: Http) { }
+  getData(): Observable<Organization[]> {
+    return this.http.get(this.orgUrl)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.organizations || {};
+  }
+  private handleError(error: Response | any) {
+    // In a real world app, you might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+}
