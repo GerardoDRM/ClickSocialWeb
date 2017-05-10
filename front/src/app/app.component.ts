c1292837c338377763b9725a3d40a12921e5129e
GerardoDRM <gerardo.delarosama@udlap.mx>
2017-05-10 02:42:53 -0500

    Fixing details

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'click-app',
   templateUrl: './app.component.html',
   styleUrls: ['./main.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
 
+  constructor(private elRef:ElementRef) {
+  }
+  ngAfterViewInit() {
+    var el = this.elRef.nativeElement.querySelector('.mdl-layout__drawer-button');
+    console.log(el);
+  }
 }
