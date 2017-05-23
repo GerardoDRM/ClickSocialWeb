e27c93d8d38650ca80f4a4ff43ae2e49bce91e8d
GerardoDRM <gerardo.delarosama@udlap.mx>
2017-05-23 16:24:13 -0500

    Adding twitter

--- front/src/app/home.component.ts
+++ front/src/app/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'click-app',
   templateUrl: './home.component.html',
   styleUrls: ['./main.css']
 })
-export class HomeComponent {
-  title = 'app works!';
+export class HomeComponent implements AfterViewInit {
+  constructor() {
+  }
+  ngAfterViewInit() {
+    !function(d, s, id) {
+      var js: any,
+        fjs = d.getElementsByTagName(s)[0],
+        p = 'https';
+      if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = p + "://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }
+    }
+      (document, "script", "twitter-wjs");
+  }
+
 }
